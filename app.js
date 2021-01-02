aeaa6b7d0f3277b14ff1349933ca4fd8290887a5
Rafal Ostrowidzki <r.ostrowidzki@gmail.com>
2021-01-02 19:29:15 +0100

    create ejs template page

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 const express = require('express'),
-      app = express(),
-      port = 3000;
+  app = express(),
+  port = 3000;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-  var today = new Date();
+  var today = new Date().getDay();
+  var day = "";
 
-  if ( today.getDay() === 6 || today.getDay() === 0 ) {
-    res.sendFile(`${__dirname}/index.html`);
-  } else {
-    res.write("<h1>Boo! I have to work!</h1>");
+  switch (today) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error");
   }
-})
+  res.render('list', {
+    day: day
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is runnig on port: ${port}`);
