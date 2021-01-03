94def75adb49cb788c54b11fee0792ee60a6fc97
Rafał Ostrowidzki <r.ostrowidzki@gmail.com>
2021-01-03 13:35:26 +0100

    handle adding items to the list

--- app.js
+++ app.js
@@ -7,38 +7,24 @@ app.use(express.urlencoded({
 }));
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  var today = new Date().getDay();
-  var day = "";
+var newItems = ["Buy Food", "Cook Food", "Eat Food"];
 
-  switch (today) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error");
-  }
+app.get("/", (req, res) => {
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
+  const date = new Date().toLocaleDateString('en-EN', options);
   res.render('list', {
-    day: day
+    date: date,
+    newItems: newItems
   });
+});
+
+app.post("/", (req, res) => {
+  newItems.push(req.body.newItem);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
