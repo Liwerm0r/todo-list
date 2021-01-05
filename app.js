72bb52d3d33109973f23fe4f6137e0949cfd3e4c
Rafal Ostrowidzki <r.ostrowidzki@gmail.com>
2021-01-05 12:59:49 +0100

    list Items are now stored in db

--- app.js
+++ app.js
@@ -4,32 +4,40 @@ const express = require('express'),
   mongoose = require('mongoose'),
   port = 3000;
 
-// set up datebase connection
-mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
-// create DB schema
-const itemSchema = new mongoose.Schema({
-  name: String
-});
-
-const Item = mongoose.model('Item', itemSchema);
-const item1 = new Item({ name: "Welcome to your todolist!" });
-const item2 = new Item({ name: "Hit the + button to add a new item." });
-const item3 = new Item({ name: "<-- Hit this to delete an item." });
-
-
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(`${__dirname}/public`));
 app.set('view engine', 'ejs');
 
+////////////////////////////////////////////////////////////////////////////////
+// ///////////////////      DB STUFF     ///////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+// create DB schema
+const itemSchema = new mongoose.Schema({ name: String });
+const Item = mongoose.model('Item', itemSchema);
 
+const item1 = new Item({ name: "Welcome to your todolist!" });
+const item2 = new Item({ name: "Hit the + button to add a new item." });
+const item3 = new Item({ name: "<-- Hit this to delete an item." });
+const defaultItems = [item1, item2, item3];
 
 // GET Requests
 
 app.get("/", (req, res) => {
-  res.render('list', {
-    listTitle: date.generateDate(),
-    newItems: items
+
+  Item.find({}, (err, items ) => {
+
+    if ( !items.length ) {
+      Item.insertMany(defaultItems, (err) => {
+        if ( err ) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items do DB.");
+        }
+      });
+      res.redirect("/");
+    }
+    res.render('list', { listTitle: date.generateDate(), newItems: items });
   });
 });
 
@@ -52,7 +60,8 @@ app.post("/", (req, res) => {
     workItems.push(req.body.newItem);
     res.redirect("/work")
   } else {
-    items.push(req.body.newItem);
+    const item = new Item({ name: req.body.newItem });
+    item.save();
     res.redirect("/");
   }
 });
