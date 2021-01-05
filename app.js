4365d14a0dd5c9f9f25283ff41571ee99279eed1
Rafal Ostrowidzki <r.ostrowidzki@gmail.com>
2021-01-05 11:23:40 +0100

    Add mongoose, create itemSchema

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express'),
   app = express(),
   date = require(`${__dirname}/date.js`),
+  mongoose = require('mongoose'),
   port = 3000;
 
-app.use(express.urlencoded({
-  extended: true
-}));
+// set up datebase connection
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+// create DB schema
+const itemSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model('Item', itemSchema);
+const item1 = new Item({ name: "Welcome to your todolist!" });
+const item2 = new Item({ name: "Hit the + button to add a new item." });
+const item3 = new Item({ name: "<-- Hit this to delete an item." });
+
+
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(`${__dirname}/public`));
 app.set('view engine', 'ejs');
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+
 
 // GET Requests
 
